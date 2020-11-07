544706532f43f7fdb6dbd0a5b62a1394488a2d6f
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2020-11-07 18:45:55 +0300

    add functionality for deleting completed todo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <Todos v-bind:todoList="todos"></Todos>
+        <Todos v-bind:todoList="todos" v-on:del-todo="deleteTodo"></Todos>
     </div>
 </template>
 
@@ -16,9 +16,14 @@ export default {
         return {
             todos: [
             {id: 1, title: 'Todo 1', completed: false},
-            {id: 1, title: 'Todo 2', completed: true},
-            {id: 1, title: 'Todo 3', completed: false}
+            {id: 2, title: 'Todo 2', completed: false},
+            {id: 3, title: 'Todo 3', completed: false}
             ]
+        }
+    },
+    methods: {
+        deleteTodo(id) {
+            this.todos = this.todos.filter(todo => todo.id !== id);
         }
     }
 }
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,19 @@
 <template>
     <div>
         <div v-bind:key="todo.id" v-for="todo in todoList">
-            <h3>{{todo.title}}</h3>
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"></TodoItem>
         </div>
     </div>
 </template>
 
 <script>
+import TodoItem from "@/components/TodoItem";
+
 export default {
     name: 'Todos',
+    components: {
+        TodoItem
+    },
     props: ['todoList']
 }
 </script>
