fd82a7ae63a130984962049fb280ba9767c250e1
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2020-11-07 18:26:22 +0300

    add new components

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,38 @@
 <template>
-  <div id="app">
-    {{msg}}
-  </div>
+    <div id="app">
+        <Todos v-bind:todoList="todos"></Todos>
+    </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
 
 export default {
-  name: 'App',
-  components: {
-
-  },
-  data() {
-    return {
-      todos: [
-        {id: 1, title: 'Todo 1', completed: false},
-        {id: 1, title: 'Todo 2', completed: true},
-        {id: 1, title: 'Todo 3', completed: false}
-      ]
+    name: 'App',
+    components: {
+        Todos
+    },
+    data() {
+        return {
+            todos: [
+            {id: 1, title: 'Todo 1', completed: false},
+            {id: 1, title: 'Todo 2', completed: true},
+            {id: 1, title: 'Todo 3', completed: false}
+            ]
+        }
     }
-  }
 }
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        line-height: 1.4;
+    }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todoList">
+            <h3>{{todo.title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Todos',
+    props: ['todoList']
+}
+</script>
+
+<style scoped>
+
+</style>
